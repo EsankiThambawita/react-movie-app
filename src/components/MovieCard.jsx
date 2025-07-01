52332740b23b58e730a279ab689167a256c121c8
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-02 01:09:32 +0530

    Added CSS styling to all the pages

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+import "../css/MovieCard.css";
+
 function MovieCard({ movie }) {
   function onFavoriteClick() {
     alert("Clicked");
@@ -9,7 +11,7 @@ function MovieCard({ movie }) {
         <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
-            ❤️
+            ♥︎
           </button>
         </div>
       </div>
